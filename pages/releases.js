e0b7b9b600983a5bac3912d6cb801a6ea6d413ee
Thirumalaa Srinivas <srinivassekar@gmail.com>
2022-01-28 08:39:53 -0600

    adds release page and slug

---
+++ pages/releases.js
@@ -0,0 +1,58 @@
+import Link from "next/link";
+import {getAllReleases} from "../lib/mdx";
+import Layout from '../components/layout';
+import Container from '../components/container';
+import Header from '../components/header';
+import PostTitle from '../components/post-title';
+import Head from 'next/head';
+import {CMS_NAME} from '../lib/constants';
+import PostHeader from '../components/post-header';
+import PostBody from '../components/post-body';
+
+export default function Releases({releases}) {
+  return (
+    <Layout>
+      <Head>
+        <title>Releases</title>
+      </Head>
+      <Container>
+        <section
+          className="flex-col md:flex-row flex items-center md:justify-between pt-8 pb-8 pl-8 pr-8 bg-gray-50 bg-transparent sticky top-0 backdrop-blur">
+          <Link href='/releases'>
+            <h1
+              className="text-3xl md:text-5xl font-bold tracking-tighter leading-tight md:pr-8 starwars-blue dark:text-red-400"
+              style={{cursor: 'pointer'}}>
+              Releases.
+            </h1>
+          </Link>
+
+
+          <Link href='/'>
+            <a className="nav-link text-xl font-regular text-stone-700">
+              Home
+            </a>
+          </Link>
+
+        </section>
+        <section
+          className="flex-col md:flex-row flex items-center md:justify-between pt-8 pb-8 pl-8 pr-8 bg-gray-50 bg-transparent text-gray-500">
+          <ul>
+            {releases.map((release, index) => (
+              <li key={index}>
+                <Link href={`releases/${release.slug}`}>{release.frontmatter.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </Container>
+    </Layout>
+  );
+}
+
+export const getStaticProps = async () => {
+  const releases = getAllReleases();
+
+  return {
+    props: {releases},
+  };
+};
---
+++ pages/releases/[slug].js
@@ -0,0 +1,72 @@
+import React from "react";
+import { getMDXComponent } from "mdx-bundler/client";
+import { getAllReleases, getSingleRelease } from "../../lib/mdx";
+import Layout from '../../components/layout';
+import Container from '../../components/container';
+import Header from '../../components/header';
+import PostTitle from '../../components/post-title';
+import Head from 'next/head';
+import {CMS_NAME} from '../../lib/constants';
+import PostHeader from '../../components/post-header';
+import PostBody from '../../components/post-body';
+import Link from 'next/link';
+
+const Release = ({ code, frontmatter }) => {
+  // console.log('slug', code, frontmatter);
+  const Component = React.useMemo(() => getMDXComponent(code), [code]);
+  return (
+    <Layout>
+      <Head>
+        <title>Next.js Blog Example with {CMS_NAME}</title>
+      </Head>
+      <Container>
+        <section
+          className="flex-col md:flex-row flex items-center md:justify-between pt-8 pb-8 pl-8 pr-8 bg-gray-50 bg-transparent sticky top-0 backdrop-blur">
+          <Link href='/releases'>
+            <h1
+              className="text-3xl md:text-5xl font-bold tracking-tighter leading-tight md:pr-8 starwars-blue dark:text-red-400"
+              style={{cursor: 'pointer'}}>
+              {frontmatter.title}
+            </h1>
+          </Link>
+
+
+          <Link href='/releases'>
+            <a className="nav-link text-xl font-regular text-stone-700">
+              Releases
+            </a>
+          </Link>
+
+        </section>
+        <section
+          className="flex-col md:flex-row flex items-center md:justify-between pt-8 pb-8 pl-8 pr-8 bg-gray-50 bg-transparent text-gray-500">
+          <div className="wrapper">
+            <h2
+              className="text-xl md:text-3xl font-bold tracking-tighter leading-tight md:pr-8 starwars-blue dark:text-red-400 mb-8"
+              style={{cursor: 'pointer'}}>
+              {frontmatter.excerpt}
+            </h2>
+            <Component />
+          </div>
+        </section>
+      </Container>
+    </Layout>
+  );
+};
+
+export const getStaticProps = async ({ params }) => {
+  const post = await getSingleRelease(params.slug);
+  return {
+    props: { ...post },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const paths = getAllReleases().map(({ slug }) => ({ params: { slug } }));
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export default Release;
