f80ddb2a9b118857cd83f1919d106caf47740282
Thirumalaa Srinivas <srinivassekar@gmail.com>
2022-01-27 21:35:18 -0600

    updates components for navigation

--- pages/index.js
+++ pages/index.js
@@ -18,17 +18,6 @@ export default function Index({ allPosts }) {
         </Head>
         <Container>
           <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
         </Container>
       </Layout>
     </>
